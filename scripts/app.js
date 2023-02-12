411ea9d1263f86234347ab0d40fbe117373fdd66
Sina Molaverdikhani <s.molaverdikhani@gmail.com>
2023-02-12 09:26:28 -0800

    main page design

---
+++ scripts/app.js
@@ -0,0 +1,27 @@
+// $(document).ready(function() {
+//   $("#folder").on("click", function() {
+//     $("#myDropdown").toggle();
+//   });
+
+//   // let $dispaly = $(".fa-folder").css("display");
+//   // if ($display === block) {
+//   //   $(".fa-folder").click(function() {
+//   //     $("#myDropdown").css("display", "none");
+//   //   });
+//   // }
+//   // Show div by removing inline display none style rule
+//   // $("#folder").click(function() {
+//   //   $("#myDropdown").css("display", "block");
+//   // });
+//   $('.fa-star').on('click', function() {
+//     $('.fa-star').css('color', 'yellow');
+//   });
+
+//   // $( "#task" ).click(function() {
+//   //   $(this).css('color', 'yellow');
+//   // })
+
+// $('h1').css('color','red');
+// $('#watch').css('color','red')
+
+// });
