29a51dd274bd220e90eab4b2941dc463eeaf2679
Sina Molaverdikhani <s.molaverdikhani@gmail.com>
2023-02-12 14:05:38 -0800

    Fixing jQuery bugs

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,13 @@
-$(document).ready(() => {
 
-  $('#folder').click(function() {
-    $('#myDropdown').slideToggle(400);
-  })
-})
+$(document).ready(() => {
+  $("#folder").click(function() {
+    $("#myDropdown").slideToggle(400);
+  });
+
+  $("#star").click(function() {
+    $(this).toggleClass("clicked");
+  });
+console.log("hello");
+}
+)
+  ;
