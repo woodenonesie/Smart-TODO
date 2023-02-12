b085861d760e1a2761044a480ed386ba7f65cc1f
Sina Molaverdikhani <s.molaverdikhani@gmail.com>
2023-02-12 11:25:39 -0800

    To make a drop down menu

--- scripts/app.js
+++ scripts/app.js
@@ -1,27 +1,6 @@
-// $(document).ready(function() {
-//   $("#folder").on("click", function() {
-//     $("#myDropdown").toggle();
-//   });
+$(document).ready(() => {
 
-//   // let $dispaly = $(".fa-folder").css("display");
-//   // if ($display === block) {
-//   //   $(".fa-folder").click(function() {
-//   //     $("#myDropdown").css("display", "none");
-//   //   });
-//   // }
-//   // Show div by removing inline display none style rule
-//   // $("#folder").click(function() {
-//   //   $("#myDropdown").css("display", "block");
-//   // });
-//   $('.fa-star').on('click', function() {
-//     $('.fa-star').css('color', 'yellow');
-//   });
-
-//   // $( "#task" ).click(function() {
-//   //   $(this).css('color', 'yellow');
-//   // })
-
-// $('h1').css('color','red');
-// $('#watch').css('color','red')
-
-// });
+  $('#folder').click(function() {
+    $('#myDropdown').slideToggle(400);
+  })
+})
