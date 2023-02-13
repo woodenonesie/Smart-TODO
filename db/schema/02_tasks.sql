0f631ce816c0e9d6af8c74a332e89b82c31304b6
Erik Anderson <108945295+ErikAndersonWebDev@users.noreply.github.com>
2023-02-12 19:58:44 -0800

    Update stock database items

--- db/schema/02_tasks.sql
+++ db/schema/02_tasks.sql
@@ -5,5 +5,5 @@ CREATE TABLE tasks (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id),
   name VARCHAR(255) NOT NULL,
-  category VARCHAR(255) NOT NULL,
-);
+  category VARCHAR(255) NOT NULL
+)
